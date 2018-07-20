9ea8f7524635b28ccb3f80bf2220307121739284
omerceder <omerceder@gmail.com>
2018-07-20 13:57:49 +0300

    Syntax

--- scripts.js
+++ scripts.js
@@ -6,21 +6,21 @@ function loadWebAssembly(fileName) {
     return fetch(fileName)
     .then(response => response.arrayBuffer())
     .then(bits => WebAssembly.compile(bits))
-   .then(module => { return new WebAssembly.Instance(module) });
+   .then(module => { return new WebAssembly.Instance(module) })
 };
 
 loadWebAssembly('squarer.wasm')
     .then(instance => {
-        squarer = instance.exports._Z7squareri;
-        console.log('Finished compiling! Ready when you are...');
-    });
+        squarer = instance.exports._Z7squareri
+        console.log('Finished compiling! Ready when you are...')
+    })
 
 // Event Handlers:
 
 document.addEventListener('DOMContentLoaded',function() {
-    document.querySelector('input[id="number-input"]').onchange=changeEventHandler;
-}, false);
+    document.querySelector('input[id="number-input"]').onchange=changeEventHandler
+}, false)
 
 function changeEventHandler( e ) {
-    document.getElementById("number-output").innerHTML = squarer(e.target.value);
+    document.getElementById("number-output").innerHTML = squarer(e.target.value)
 }
